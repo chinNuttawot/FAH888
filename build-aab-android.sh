7f518ca8b80db515a10a854a8c9ef73382547d00
[Mac Studio]chin <znuttawot.s@pttdigital.com>
2024-02-02 19:44:50 +0700

    add keystore

---
+++ build-aab-android.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## single build : ./build-android.sh qa
+## multiple build : ./build-android.sh qa,develop,uat,production
+
+npx jetify
+
+IN=$1
+apk_path="/Users/chin/FAH888"
+
+envs=$(echo $IN | tr "," "\n")
+
+for env in $envs
+do
+    rm -r $(echo "android/app/build/outputs/bundle/release/app-release.aab")
+    cd android
+    ./gradlew clean
+    ./gradlew $(echo "bundleRelease")
+    cd ..
+    appVersion="1.0.0"
+    buildTime="$(date +'%d.%b.%y-%H.%M')"
+    cp -f $(echo "android/app/build/outputs/bundle/release/app-release.aab")  $(echo "$apk_path/fah888-${appVersion}-${buildTime}.aab")
+done
